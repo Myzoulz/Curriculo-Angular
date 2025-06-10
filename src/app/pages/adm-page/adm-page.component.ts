4ec93c9ec0576fc60cf41cb71486a784d8d3416c
Myzoulz <lucasrocha7455@gmail.com>
2025-06-10 14:15:19 -0300

    start

---
+++ src/app/pages/adm-page/adm-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adm-page',
+  imports: [],
+  templateUrl: './adm-page.component.html',
+  styleUrl: './adm-page.component.css'
+})
+export class AdmPageComponent {
+
+}
